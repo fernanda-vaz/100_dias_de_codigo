b149db8d3bf6d7e07e05f7d5012a9fa26a5e14bc
Fernanda Vaz <flauta.fernanda@gmail.com>
2024-09-27 16:55:54 -0300

    dia 13

---
+++ dia-13/script.js
@@ -0,0 +1,24 @@
+setInterval(atualizarRelogio, 1000);
+
+function atualizarRelogio() {
+  let data = new Date();
+  let horas = data.getHours();
+  let minutos = data.getMinutes();
+  let segundos = data.getSeconds();
+
+  if (horas < 10) {
+    horas = "0" + horas;
+  }
+
+  if (minutos < 10) {
+    minutos = "0" + minutos;
+  }
+
+  if (segundos < 10) {
+    segundos = "0" + segundos;
+  }
+
+  let relogioDigital = horas + ":" + minutos + ":" + segundos;
+
+  document.getElementById("clock").innerHTML = relogioDigital;
+}
